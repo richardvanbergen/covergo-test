f2123e8b984285f103edfea34feb5a921daddbe5
Richard Vanbergen <rich.vanbergen@gmail.com>
2022-01-26 14:35:25 +0900

    Adding icons to notifications.
    Include phospor icons and tweak some notification styles.

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,6 @@ import ToastNotification from './components/layout/ToastNotification.vue'
 import MainHeader from './components/layout/MainHeader.vue'
 import MainFooter from './components/layout/MainFooter.vue'
 import LayoutContainer from './components/layout/LayoutContainer.vue'
-import { notify } from './lib/notify'
-
-setTimeout(() => {
-  console.log('notify')
-  notify('success', 'Success', 'You are a winner.')
-})
-
-setTimeout(() => {
-  console.log('error')
-  notify('error', 'Error', 'You broke something, idiot.')
-}, 3000)
 </script>
 
 <template>
@@ -26,6 +15,8 @@ setTimeout(() => {
 
   <ToastNotification notification-type="success" />
   <ToastNotification notification-type="error" />
+  <ToastNotification notification-type="warning" />
+  <ToastNotification notification-type="notice" />
 </template>
 
 <style>
--- src/components/layout/ToastNotification.vue
+++ src/components/layout/ToastNotification.vue
@@ -1,76 +1,50 @@
 <script setup lang="ts">
 import { NotificationType } from '@/lib/notify'
 import { computed } from 'vue'
+import { PhWarning, PhSmiley, PhHandWaving, PhSmileySad } from 'phosphor-vue'
 
 const props = defineProps<{
   notificationType: NotificationType
 }>()
 
-const colors = computed(() => {
+const notificationStyle = computed(() => {
   switch (props.notificationType) {
     case 'success':
       return {
         bg: 'bg-green-500',
         text: 'text-green-500',
         border: 'border-green-500',
+        icon: PhSmiley,
       }
     case 'error':
       return {
         bg: 'bg-red-500',
         text: 'text-red-500',
         border: 'border-red-500',
+        icon: PhSmileySad,
       }
     case 'warning':
       return {
         bg: 'bg-orange-500',
         text: 'text-orange-500',
         border: 'border-orange-500',
+        icon: PhWarning,
       }
     default:
       return {
         bg: 'bg-blue-500',
         text: 'text-blue-500',
         border: 'border-blue-500',
+        icon: PhHandWaving,
       }
   }
 })
 </script>
 
 <template>
-  <div
-    class="fixed inset-0 flex items-end justify-end p-6 px-4 py-6 pointer-events-none"
-  >
-    <div class="w-full max-w-sm">
-      <div
-        class="flex w-full max-w-sm mx-auto mt-4 overflow-hidden bg-white rounded-lg shadow-lg border border-red-500"
-      >
-        <div class="flex items-center justify-center w-12" :class="colors.bg">
-          <svg
-            class="w-6 h-6 text-white fill-current"
-            viewBox="0 0 40 40"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z"
-            />
-          </svg>
-        </div>
-
-        <div class="px-4 py-2">
-          <div class="mx-3">
-            <span class="font-semibold leading-none" :class="colors.text"
-              >Title</span
-            >
-            <p class="text-sm text-gray-600">Some other text</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <NotificationGroup :group="notificationType">
     <div
-      class="fixed inset-0 flex items-end justify-end p-6 px-4 py-6 pointer-events-none"
+      class="fixed inset-0 flex items-end justify-end p-6 pointer-events-none"
     >
       <div class="w-full max-w-sm">
         <Notification
@@ -86,31 +60,31 @@ const colors = computed(() => {
         >
           <div
             class="flex w-full max-w-sm mx-auto mt-4 overflow-hidden bg-white rounded-lg shadow-lg border"
-            :class="colors.border"
+            :class="notificationStyle.border"
             v-for="notification in notifications"
             :key="notification.id"
           >
             <div
-              class="flex items-center justify-center w-12"
-              :class="colors.bg"
+              class="flex items-center justify-center py-2 px-4"
+              :class="notificationStyle.bg"
             >
-              <svg
-                class="w-6 h-6 text-white fill-current"
-                viewBox="0 0 40 40"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z"
-                />
-              </svg>
+              <component
+                :is="notificationStyle.icon"
+                class="text-white"
+                size="35"
+              />
             </div>
 
             <div class="px-4 py-2">
               <div class="mx-3">
-                <span class="font-semibold leading-none" :class="colors.text">{{
-                  notification.title
-                }}</span>
-                <p class="text-sm text-gray-600">{{ notification.text }}</p>
+                <span
+                  class="font-semibold leading-none"
+                  :class="notificationStyle.text"
+                  >{{ notification.title }}</span
+                >
+                <p class="text-sm text-gray-600 mb-2">
+                  {{ notification.text }}
+                </p>
               </div>
             </div>
           </div>
